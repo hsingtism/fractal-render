48209c8a3138820345a8e30c3e902f3c0a70a165
Hsing Lo <hsing@flyingbat.net>
2022-11-23 17:41:30 -0500

    hsl to rgb

--- fractal.c
+++ fractal.c
@@ -2,10 +2,20 @@
 #include <stdio.h>
 #include <complex.h>
 #include <math.h>
+#include <stdint.h>
+
+#define BLUE 0
+#define GREEN 1
+#define RED 2
+
+// use only for debug
+void printComplex(complex double z) {
+    printf("%f + i*%f\n", creal(z), cimag(z));
+}
 
 void renderFrame(complex double topleft, complex double bottomright, complex double c, int width, int height, int seqID, int maxIteration);
-void printComplex(complex double z);
 int iterate(complex double z, complex double c, int maxIteration);
+uint32_t hsl2rgb(double h, double s, double l);
 complex double iterator(complex double x, complex double c);
 complex double mandelbrot(complex double x, complex double c);
 complex double polynomial(complex double x, complex double *coefficents, int degree);
@@ -36,9 +46,9 @@ void renderFrame(complex double topleft, complex double bottomright, complex dou
                 + pixelDeltaH * w;
             int pixel = iterate(c, position, maxIteration); //TODO depending on of z or c is changed, this needs to be switched
             //TODO iterate and generate colors here
-            image[h][w][0] = pixel & 1 ? 0x7F : 0xFF;
-            image[h][w][1] = pixel & 1 ? 0x7F : 0xFF;
-            image[h][w][2] = pixel & 1 ? 0x7F : 0xFF;
+            image[h][w][BLUE]  = pixel & 1 ? 0x7F : 0xFF; // blue
+            image[h][w][GREEN] = pixel & 1 ? 0x7F : 0xFF; // green
+            image[h][w][RED]   = pixel & 1 ? 0x7F : 0xFF; // red
         }
     }
 
@@ -57,8 +67,33 @@ void renderFrame(complex double topleft, complex double bottomright, complex dou
     printf("%s rendered\n", filename);
 }
 
-void printComplex(complex double z) {
-    printf("%f + i*%f\n", creal(z), cimag(z));
+// based on https://stackoverflow.com/a/9493060/15879600
+double hsl2rgb_internal_1(double p, double q, double t) {
+    if(t < 0) t += 1;
+    if(t > 1) t -= 1;
+    if(t < 0.16666666666666666) return p + (q - p) * 6 * t;
+    if(t < 0.50000000000000000) return q;
+    if(t < 0.66666666666666666) return p + (q - p) * (0.66666666666666666 - t) * 6;
+    return p;  
+}
+
+uint32_t hsl2rgb(double h, double s, double l) { 
+    uint32_t rgb = 0x0000006D;
+    // using bit hacking because it is way less effort.
+    // first 8 MSB is red, then green, then blue. last 8 bit not used
+    if (s < 0.001953125) { // s is 0
+        int lval = (unsigned char)round(l * 255);
+        return rgb | (lval << 8) | (lval << 16) | (lval << 24);
+    }
+
+    double q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    double p = 2 * l - q;
+    return (
+        rgb
+        | ((unsigned char)round(hsl2rgb_internal_1(p, q, h - 0.3333333333333333)) << 8)
+        | ((unsigned char)round(hsl2rgb_internal_1(p, q, h                     )) << 16)
+        | ((unsigned char)round(hsl2rgb_internal_1(p, q, h + 0.3333333333333333)) << 24)
+    );
 }
 
 int iterate(complex double z, complex double c, int maxIteration) {
