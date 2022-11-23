cf50478adf82282b1f661053f5bcef39ffea0f66
Hsing Lo <hsing@flyingbat.net>
2022-11-22 21:13:23 -0500

    main backbone prototyped

--- js-prototype/script.js
+++ js-prototype/script.js
@@ -1,3 +1,8 @@
+const variableVariable = 1 // 1 is julia (changing seed), 0 is fatou (changing function)
+// const constantValue = [0, 0]
+const constantValue = [0.262, 0.454]
+const iteratorFunction = (a, b) => addComplex(squareComplex(a), b)
+
 const ctx = document.getElementById('mainp').getContext('2d')
 const addComplex = (a, b) => [a[0] + b[0], a[1] + b[1]]
 const squareComplex = (a) => [a[0] * a[0] - a[1] * a[1], 2 * a[0] * a[1]]
@@ -12,7 +17,6 @@ for(let i = 0; i < 600; i++) {
     for(let j = 0; j < 600; j++) {
         let re = -2 + i / 150
         let im = -2 + j / 150
-        // let color = classicF([re, im])
         let color = classicJ([re, im])
         drawPixel(i, j, color)
     }
@@ -20,30 +24,13 @@ for(let i = 0; i < 600; i++) {
 
 
 function classicJ(x) {
-    let c = [0.262, 0.454]
-    let z = x
+    let c = variableVariable ? constantValue : x
+    let z = variableVariable ? x : constantValue
 
     for (let i = 0; i < 10000; i++) {
         let zf = z
-        z = addComplex(squareComplex(z), c)
+        z = iteratorFunction(z, c)
         if (absComplex(z) > 1000) {
-            return 'gray'
-        }
-        if (zf[0] == z[0] && zf[1] == z[1]) {
-            return 'black'
-        }
-    }
-    return 'black'
-}
-
-function classicF(x) {
-    let c = x
-    let z = [0, 0]
-
-    for (let i = 0; i < 1000; i++) {
-        let zf = z
-        z = addComplex(squareComplex(z), c)
-        if (absComplex(z) > 2) {
             return 'gray'
         }
         if (zf[0] == z[0] && zf[1] == z[1]) {
