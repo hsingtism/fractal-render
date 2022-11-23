f1c1f8de5a193ed5f41b2cf565e5e23c08a476ce
Hsing Lo <hsing@flyingbat.net>
2022-11-22 19:05:26 -0500

    js generates simple mandelbrot set

--- js-prototype/script.js
+++ js-prototype/script.js
@@ -1,8 +1,36 @@
 const ctx = document.getElementById('mainp').getContext('2d')
+const addComplex = (a, b) => [a[0] + b[0], a[1] + b[1]]
+const squareComplex = (a) => [a[0] * a[0] - a[1] * a[1], 2 * a[0] * a[1]]
+const absComplex = (a) => Math.sqrt(a[0] * a[0] + a[1] * a[1])
+
+function drawPixel(x, y, color) {
+    ctx.fillStyle = color
+    ctx.fillRect(x, y, 1, 1)
+}
 
 for(let i = 0; i < 600; i++) {
-    ctx.fillStyle = (i % 4) ? 'red' : 'black'
     for(let j = 0; j < 600; j++) {
-        ctx.fillRect(i,j,1,1)
+        let re = -2 + i / 150
+        let im = -2 + j / 150
+        let color = classicF([re, im])
+        drawPixel(i, j, color)
     }
+}
+
+
+function classicF(x) {
+    let c = x
+    let z = [0, 0]
+
+    for (let i = 0; i < 1000; i++) {
+        let zf = z
+        z = addComplex(squareComplex(z), c)
+        if (absComplex(z) > 2) {
+            return 'gray'
+        }
+        if (zf[0] == z[0] && zf[1] == z[1]) {
+            return 'black'
+        }
+    }
+    return 'black'
 }
