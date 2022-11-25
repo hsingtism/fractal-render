dc7df6edb99a18ebe8250cb92fa43c5980f333d5
Hsing Lo <hsing@flyingbat.net>
2022-11-25 01:02:41 -0500

    rename and cleanup

--- fractal.c
+++ fractal.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /* settings.c */   cplxdbl iterator(cplxdbl x, cplxdbl c);
-/* self */         void renderFrame(cplxdbl topleft, cplxdbl bottomright, cplxdbl secondParameter, byte mode, int width, int height, int seqID, int maxIteration);
+/* self */         void renderFrame(cplxdbl topleft, cplxdbl bottomright, cplxdbl constant, byte mode, int width, int height, int seqID, int maxIteration);
 /* auxiliaryFunctions.c */uint32_t hsl2rgb(double h, double s, double l);
 /* self */         uint64_t iterate(cplxdbl z, cplxdbl c, int maxIteration, cplxdbl previous, char initialCall);
 /* settings.c */   cplxdbl* colorTableData(cplxdbl* table);
@@ -14,7 +14,7 @@ this giant function manages everything
 renders and save an image
   - topleft - complex number of the image's top left
   - bottomright - complex numer of the image's bottom right 
-  - secondParameter
+  - constant
   - mode whether to iterate
   - width - image width in pixels
   - height - image height in pixels
@@ -22,7 +22,7 @@ renders and save an image
   - maxIteration - the maximum amount of iteration for each pixel
 
 */
-void renderFrame(cplxdbl topleft, cplxdbl bottomright, cplxdbl secondParameter, byte mode, int width, int height, int seqID, int maxIteration) {
+void renderFrame(cplxdbl topleft, cplxdbl bottomright, cplxdbl constant, byte mode, int width, int height, int seqID, int maxIteration) {
     // byte image[height][width][3]; 
     // must be on heap to prevent stack overflow
     byte * image = malloc(height * height * 3 * sizeof(byte));
@@ -30,18 +30,20 @@ void renderFrame(cplxdbl topleft, cplxdbl bottomright, cplxdbl secondParameter, 
     cplxdbl imagesize = bottomright - topleft;
     cplxdbl pixelDeltaV = cimag(imagesize) / height;
     cplxdbl pixelDeltaH = creal(imagesize) / width;
+    
     for (int h = 0; h < height; h++) {
         for(int w = 0; w < width; w++) {
             cplxdbl position = topleft 
                 + pixelDeltaV * h * I
                 + pixelDeltaH * w;
-            uint64_t pixel = mode ? iterate(position, secondParameter, maxIteration, 0, 1) : iterate(secondParameter, position, maxIteration, 0, 1);
+            uint64_t pixel = mode ? iterate(position, constant, maxIteration, 0, 1) : iterate(constant, position, maxIteration, 0, 1);
             uint32_t color = hsl2rgb(setfpbits32(pixel), IMAGE_SATURATION, setfpbits32(pixel >> 32)); // be careful, lots of auto type casting
             image[3 * width * h + 3 * w + BLUE]  = color >> 8;
             image[3 * width * h + 3 * w + GREEN] = color >> 16;
             image[3 * width * h + 3 * w + RED]   = color >> 24; // TODO program crashes here for certain image dimensions
         }
     }
+
     char filename[13];
     for (char exp = 7; exp > -1; exp--) { // base conversion
         filename[exp] = (byte)(48 + seqID % 10);
@@ -52,6 +54,7 @@ void renderFrame(cplxdbl topleft, cplxdbl bottomright, cplxdbl secondParameter, 
     filename[10] = (byte)('m');
     filename[11] = (byte)('p');
     filename[12] = (byte)(0);
+
     generateBitmapImage((byte *)image, height, width, filename);
     free(image);
     printf("%s rendered\n", filename);
