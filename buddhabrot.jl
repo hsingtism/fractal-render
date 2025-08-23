773adbcdbfea5b1d015e162affbdf018043b8358
Hsing Lo <hsing@flyingbat.net>
2025-08-23 18:30:22 -0400

    allow mapping of value right before escape, added antibuddhabrot

--- buddhabrot.jl
+++ buddhabrot.jl
@@ -6,7 +6,11 @@ function buddhabrotSampleMap(c::ComplexF64, maxIter)
 
     for i in 1:maxIter
         z = z * z + c
+        if abs(real(z)) > 2 || abs(imag(z)) > 2
+            return visitedPoints
+        end
         if abs(z) > 2
+            push!(visitedPoints, z)
             return visitedPoints
         end
         push!(visitedPoints, z)
@@ -15,25 +19,37 @@ function buddhabrotSampleMap(c::ComplexF64, maxIter)
     return ComplexF64[] # does not escape, in the set
 end
 
+function antibuddhabrotSampleMap(c::ComplexF64, maxIter)
+    z = ComplexF64(0)
+    visitedPoints = ComplexF64[]
+
+    for i in 1:maxIter
+        z = z * z + c
+        if abs(z) > 2
+            return ComplexF64[]
+        end
+        push!(visitedPoints, z)
+    end
+
+    return visitedPoints
+end
+
 function buddhabrot(maxIterations, additionalPointFactor = 1, center = 0.0+0.0im, radiusH = 2,
     heightPixels = 1000, widthPixels = 1000,  
-    filename = "buddhabrot.png")
+    filename = "antibuddhabrot.png")
 
     radiusW = radiusH * widthPixels / heightPixels
     
     coordinatesSampleMatrix = coordinateMatrix(center, radiusW, radiusH, widthPixels * additionalPointFactor, heightPixels * additionalPointFactor)
     coordinatesSampleVec = vec(coordinatesSampleMatrix)
     visitedValuesMatrix = ThreadsX.map(x -> buddhabrotSampleMap(x, maxIterations), coordinatesSampleVec)
-    # visitedValuesMatrix = ThreadsX.map(x -> x, coordinatesSampleVec)
     visitedValues = visitedValuesMatrix |> Iterators.flatten |> collect
-
+    # TODO prevent too much memory use
     
     coordinatesImage = coordinateMatrix(center, radiusW, radiusH, widthPixels, heightPixels)
     imageCoordReal = coordinatesImage[1, :] .|> real
     imageCoordImag = coordinatesImage[:, 1] .|> imag
     incrementIndex = visitedValues .|> visited -> [findfirst(t -> t <= imag(visited), imageCoordImag), findfirst(t -> t >= real(visited), imageCoordReal)]
-    # println(visitedValues[3000:5495])
-    # println(incrementIndex[3000:5495])
 
     pixelVals = coordinatesImage .|> x -> 0
     incrementIndex .|> v -> pixelVals[v...] += 1 # ! mutation
